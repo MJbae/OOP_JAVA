7e0d8b12683fb28f16f46aa6bdca8820faecf1db
MJbae <bmj5208@gmail.com>
2021-01-01 21:37:51 +0900

    Init Project

---
+++ src/main/java/GentleM/oop_java/OopJavaApplication.java
@@ -0,0 +1,13 @@
+package GentleM.oop_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OopJavaApplication.class, args);
+	}
+
+}
