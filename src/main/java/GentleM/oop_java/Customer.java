479bd0f2135a959928aa6a0f273bb26696b87e93
MJbae <bmj5208@gmail.com>
2021-01-02 22:01:06 +0900

    Refactor: Customer, Menu, MenuItem class 관계 구현

--- src/main/java/GentleM/oop_java/Customer.java
+++ src/main/java/GentleM/oop_java/Customer.java
@@ -2,8 +2,26 @@ package GentleM.oop_java;
 
 import GentleM.oop_java.Interface.ICustomer;
 
+import java.util.ArrayList;
+
 public class Customer implements ICustomer {
     @Override
-    public void order(String coffeeName) {
+    public void order(String coffeeName, Menu menu) {
+        MenuItem orderedItem = menu.searchMenuItem(coffeeName);
+        System.out.println(orderedItem.getName() + " is ordered");
+    }
+
+    public static void main(String[] args) {
+        Customer customer = new Customer();
+
+        ArrayList<MenuItem> items = new ArrayList<MenuItem>();
+        items.add(new MenuItem("americano", 10));
+        items.add(new MenuItem("milktea", 5));
+        items.add(new MenuItem("milk", 2));
+        items.add(new MenuItem("water", 1));
+
+        Menu menu = new Menu(items);
+
+        customer.order("milk", menu);
     }
 }
--- src/main/java/GentleM/oop_java/Interface/ICustomer.java
+++ src/main/java/GentleM/oop_java/Interface/ICustomer.java
@@ -1,5 +1,7 @@
 package GentleM.oop_java.Interface;
 
+import GentleM.oop_java.Menu;
+
 public interface ICustomer {
-    public void order(String coffeeName);
+    public void order(String coffeeName, Menu menu);
 }
